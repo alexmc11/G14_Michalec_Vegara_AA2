bf3b2590eb8cce1e2addcfc09beb0e0752b104f9
Albert Vegara Martínez <albert.vegara@gmail.com>
2017-12-08 12:32:43 +0100

    Mierda

--- src/G14_AA2/Menu.h
+++ src/G14_AA2/Menu.h
@@ -7,6 +7,14 @@ public:
 	Menu();
 	~Menu();
 	SDL_Rect bgMenu;
+	/*TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
+
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "My first SDL game", SDL_Color{ 255, 150, 0, 255 }) };
+
+	//SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(m_renderer, tmpSurf) };
+
+	SDL_Rect textRect{ 100, 50, tmpSurf->w, tmpSurf->h };*/
+
 	void Update() override;
 	void Draw() override;
 	void HandleEvents() override;
