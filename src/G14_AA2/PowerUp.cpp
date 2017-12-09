cbbd17d19c0f983b74de119f522211dd42e961a6
alex michalec <alexmc11@hotmail.es>
2017-12-09 12:01:47 +0100

    powerup

--- src/G14_AA2/PowerUp.cpp
+++ src/G14_AA2/PowerUp.cpp
@@ -10,3 +10,18 @@ PowerUp::PowerUp()
 PowerUp::~PowerUp()
 {
 }
+
+void PowerUp::createPowerUp(int posX, int posY)
+{
+	powerupRect = { posX, posY, 48, 48 };
+	int result = rand() % 1;
+	if (result == 1)
+	{
+		powerupTarget = { 48, 48, 48, 48 };
+	}
+	else
+	{
+		powerupTarget = { 96, 48, 48, 48 };
+	}
+	Renderer::Instance()->PushSprite(ITEMS_SPRITE, powerupTarget, powerupRect);
+}
--- src/G14_AA2/PowerUp.h
+++ src/G14_AA2/PowerUp.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <SDL.h>
+#include "Renderer.h"
 class PowerUp
 {
 public:
+	SDL_Rect powerupRect, powerupTarget;
 	PowerUp();
 	~PowerUp();
+	void createPowerUp(int posX, int posY);
 };
 
